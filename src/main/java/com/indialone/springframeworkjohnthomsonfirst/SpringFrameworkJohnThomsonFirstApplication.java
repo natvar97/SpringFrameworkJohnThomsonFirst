f24fcdfe8fe6aa9d6e564b98951612bfdc29ede5
Natavar Prajapati <prajapatinatavar21197@gmail.com>
2021-07-08 08:55:12 +0530

    Initial commit

---
+++ src/main/java/com/indialone/springframeworkjohnthomsonfirst/SpringFrameworkJohnThomsonFirstApplication.java
@@ -0,0 +1,13 @@
+package com.indialone.springframeworkjohnthomsonfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkJohnThomsonFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkJohnThomsonFirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/indialone/springframeworkjohnthomsonfirst/SpringFrameworkJohnThomsonFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indialone.springframeworkjohnthomsonfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkJohnThomsonFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
